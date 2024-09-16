73b8a8036b3c57e867e9e0f9151d69fb3fcecf67
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-16 22:46:33 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import config
 import decimal
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from loader import bot, cryptopay
+import aiocryptopay
 import aiosqlite
 import random
 import hashlib
@@ -11,9 +12,9 @@ from datetime import datetime
 
 m = lambda num: decimal.Decimal(f"{num}")
 
-   
+
 def is_float(s):
-    if re.match("^d+?.d+?$", s) is None:
+    if re.match("^\d+?\.\d+?$", s) is None:
         return s.isdigit()
     return True
 
@@ -25,22 +26,25 @@ async def get_user_balance(user_id):
             result = await cursor.fetchone()
             return result[0] if result else None
 
+
 async def update_user_balance(user_id, new_balance):
     async with aiosqlite.connect('database.sqlite') as db:
         async with db.cursor() as cursor:
             await cursor.execute("UPDATE user SET balance=? WHERE user_id=?", (new_balance, user_id))
             await db.commit()
 
-async def lose_notify(msg: str, photo: str, zalupa : float, user_id: str):
+
+async def lose_notify(msg: str, photo: str, zalupa: float, user_id: str):
     datatest = Decimal(zalupa)
     sobaka = datatest * Decimal('0.04')
-    sobaka_str = str(sobaka.quantize(Decimal('0.00')))  # Форматируем число так, чтобы оно всегда имело два знака после запятой
+    sobaka_str = str(
+        sobaka.quantize(Decimal('0.00')))  # Форматируем число так, чтобы оно всегда имело два знака после запятой
 
     current_time = datetime.now().isoformat()
     hash_input = f"{user_id}{zalupa}{current_time}".encode('utf-8')
     hash_object = hashlib.sha256(hash_input)
     hash_hex = hash_object.hexdigest()
-    
+
     # Получаем текущий баланс пользователя
     current_balance = await get_user_balance(user_id)
     # Если баланс равен None, устанавливаем его равным 0
@@ -52,26 +56,27 @@ async def lose_notify(msg: str, photo: str, zalupa : float, user_id: str):
     await update_user_balance(user_id, str(new_balance))
 
     return await bot.send_photo(
-    config.CHANNEL_ID,
-    photo,
-    f"<b>Вы проиграли.</b>nn"
-    "<blockquote>"
-    "Попытай свою удачу снова!nЖелаю удачи в следующих ставках!"
-    "</blockquote>nn"
-    f"<b>• Вам начислен кэшбек {sobaka_str} $ на баланс бота.</b>nn"
-    f"Хэш игры: <code> {hash_hex} </code>nn"
-    f'<a href="{config.FAQ_LINK}">Как сделать ставку?</a> | '
-    f'<a href="{config.SUPPORT_LINK}">Тех. поддержка</a> | '
-    f'<a href="{config.RULES_LINK}">Правила</a> | '
-    f'<a href="{config.NEWS_LINK}">Новостной канал</a>',    reply_markup=InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(
-                "⚡️ Сделать ставку",
-                config.INVOICE_LINK
-            )],
-        ]
+        config.CHANNEL_ID,
+        photo,
+        f"<b>Вы проиграли.</b>\n\n"
+        "<blockquote>"
+        "Попытай свою удачу снова!\nЖелаю удачи в следующих ставках!"
+        "</blockquote>\n\n"
+        f"<b>• Вам начислен кэшбек {sobaka_str} $ на баланс бота.</b>\n\n"
+        f"Хэш игры: <code> {hash_hex} </code>\n\n"
+        f'<a href="{config.FAQ_LINK}">Как сделать ставку?</a> | '
+        f'<a href="{config.SUPPORT_LINK}">Тех. поддержка</a> | '
+        f'<a href="{config.RULES_LINK}">Правила</a> | '
+        f'<a href="{config.NEWS_LINK}">Новостной канал</a>',
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(
+                    "⚡️ Сделать ставку",
+                    config.INVOICE_LINK
+                )],
+            ]
+        )
     )
-)
 
 
 async def win_notify(sum: float, msg: str, photo: str):
@@ -102,6 +107,7 @@ async def win_notify(sum: float, msg: str, photo: str):
         )
     )
 
+
 async def nic_notify(msg: str, photo: str, bid: float, user_id: str):
     bids = Decimal(bid)
     bidchlen = round(bid * 0.93, 2)
@@ -123,12 +129,14 @@ async def nic_notify(msg: str, photo: str, bid: float, user_id: str):
         f'<a href="{config.NEWS_LINK}">Новостной канал</a></b>',
         reply_markup=InlineKeyboardMarkup(
             inline_keyboard=[
-                [InlineKeyboardButton(                    "⚡️ Сделать ставку",
+                [InlineKeyboardButton(
+                    "⚡️ Сделать ставку",
                     config.INVOICE_LINK
                 )],
             ]
         )
     )
+
 
 async def draw_notify(msg: str, photo: str):
     return await bot.send_photo(
@@ -157,7 +165,7 @@ def parse_data(msg: Message) -> dict[str, int | str]:
         bid_text = re.search(r'\(\$(.*?)\)', msg.text.split('\n')[0].split()[-1])
         bid = float(bid_text.group(1)) if bid_text else 0.0
         comment = ' '.join(msg.text.split('\n')[-1].split()[1::]).lower()
-        
+
         return {
             "user_id": user_id,
             "name": name,
@@ -166,9 +174,8 @@ def parse_data(msg: Message) -> dict[str, int | str]:
         }
     except (AttributeError, IndexError, ValueError):
         return {}
-
-
-async def notify_bid(data: dict, game):= InlineKeyboardMarkup(row_width=1)
+async def notify_bid(data: dict, game)
+    kb = InlineKeyboardMarkup(row_width=1)
     kb.add(
         InlineKeyboardButton(
             "✅ Одобрить",
